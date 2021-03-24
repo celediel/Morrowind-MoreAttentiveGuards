845d5497603c19e81ffdc1dd0b8e5669474fb0f8
Lilian Jónsdóttir <lilian.jonsdottir@gmail.com>
2021-03-23 18:56:06 -0700

    version bump

--- MWSE/mods/celediel/MoreAttentiveGuards/common.lua
+++ MWSE/mods/celediel/MoreAttentiveGuards/common.lua
@@ -4,7 +4,7 @@ local this = {}
 
 this.modName = "More Attentive Guards" -- or something
 this.author = "Celediel"
-this.version = "1.1.5"
+this.version = "1.1.6"
 this.modInfo = "Guards with some actual spatial awareness!\n\n" ..
 "Guards who catch you sneaking will follow you for a bit of" ..
 "time, and will also come to the player's rescue if attacked unprovoked."
